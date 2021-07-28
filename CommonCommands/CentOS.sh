bb0153091fca17cdf472b206ece3af57ac9897e3
bugpz <13558622779@163.com>
2021-07-28 20:59:21 +0800

    增加命令

--- CommonCommands/CentOS.sh
+++ CommonCommands/CentOS.sh
@@ -1,3 +1,6 @@
+#防火墙状态
+systemctl status firewalld
+
 #启动防火墙
 systemctl  start firewalld
 
@@ -13,4 +16,28 @@ firewall-cmd --query-port=xx/tcp
 #开放端口  开放后需重启防火墙才能生效   后面加此参数永久生效 --permanent
 firewall-cmd --add-port=xx/tcp
 
+#查看进程 xxx 程序名
+ps -x |grep xxx
 
+#=================================================================================================================================
+#docker 常用命令
+#拉取镜像  以nginx为例
+docker pull nginx
+#删除镜像 rmi: remove image   xxx镜像名  镜像tag不为latest时必填
+docker rmi xxx:tag
+#查看镜像
+docker images
+
+#启动服务 参数 -p xx:xxx  端口映射   -v  xx:xxx 资源映射  -name  服务命名  --restart=always  自动重启 -d 后台运行
+docker run xxx
+#停服务
+docker stop xxx
+#删除服务
+docker rm xxx
+#查看服务  -a 参数 查看所有服务
+docker ps
+
+#下载文件 wget + 链接
+wget http://nginx.org/download/nginx-1.13.7.tar.gz
+访问网站 curl
+curl www.baidu.com
