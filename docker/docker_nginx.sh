7feff9565761ca0e2015e36df549dcd0a49d1b54
bugpz <13558622779@163.com>
2021-07-30 16:13:30 +0800

    fix: 修复挂载失败的问题

--- docker/docker_nginx.sh
+++ docker/docker_nginx.sh
@@ -2,8 +2,25 @@
 echo "==============================wrote by The-Ruffian======================"
 mkdir -p /data/wwwroot/nginx
 cd /data/wwwroot/nginx
-echo -e "FROM nginx:latest\nMAINTAINER the-ruffian">Dockerfile
-docker build -t nginx:1 .
-echo "===============================build SUCCESS============================="
-docker rmi nginx
-docker images
+mkdir -p {conf,html,logs}
+
+echo "============================================================"
+docker run -d --name nginx -p 8080:8080 nginx
+docker cp nginx:/etc/nginx/nginx.conf ./
+docker cp nginx:/etc/nginx/conf.d/default.conf ./conf/
+docker stop nginx
+docker rm -f nginx
+
+echo "==========================================================start nginx============================================================="
+docker run -d --name nginx -p 18080:80 -v /data/wwwroot/nginx/nginx.conf:/etc/nginx/nginx.conf -v /data/wwwroot/nginx/logs:/var/log/nginx -v /data/wwwroot/nginx/html/:/usr/share/nginx/html -v /data/wwwroot/nginx/conf/:/etc/nginx/conf.d --privileged=true nginx
+docker ps
+
+#docker run -d  后台运行
+# --name nginx  服务取名
+# -p 8089:80  端口映射
+# -v /data/wwwroot/nginx/nginx.conf:/etc/nginx/nginx.conf 全局配置挂载
+#-v /data/wwwroot/nginx/logs:/var/log/nginx 日志挂载
+#-v /data/wwwroot/nginx/html/:/usr/share/nginx/html  资源挂载
+#-v /data/wwwroot/nginx/conf/:/etc/nginx/conf.d 配置挂载（default.conf）
+#--privileged=true 容器内部对挂载的目录拥有读写等特权
+#nginx 要启动的镜像名
