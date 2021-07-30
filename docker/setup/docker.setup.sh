0618a2d77130e94cd85178bf4094e6787a942c0d
bugpz <13558622779@163.com>
2021-07-30 13:34:06 +0800

    add：docker安装

--- docker/setup/docker.setup.sh
+++ docker/setup/docker.setup.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
-yum install -y docker-ce docker-ce-cli containerd.io
+yum update -y
+
+yum install -y yum-utils #管理yum源
+
+yum-config-manager --add-repo http://mirrors.aliyun.com/docker-ce/linux/centos/docker-ce.repo #添加yum源
+
+yum makecache fast #更新索引
+
+yum install -y docker-ce
 
 echo "==============================报错请运行 fix.sh======================="
 
--- docker/setup/fix.sh
+++ docker/setup/fix.sh
@@ -1,12 +1,5 @@
 #!/bin/bash
 echo ====================start repair=========================""
-yum update -y
-
-yum install -y yum-utils #管理yum源
-
-yum-config-manager --add-repo http://mirrors.aliyun.com/docker-ce/linux/centos/docker-ce.repo #添加yum源
-
-yum makecache fast #更新索引
 
 yum install -y epel-release #阿里云上的epel源
 yum install -y  container-selinux
